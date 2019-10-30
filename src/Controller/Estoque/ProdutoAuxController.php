1c4b46088cf56ed308a7b43d21c9a5413c43b2dd
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-10-29 22:28:32 -0300

    ...

--- src/Controller/Estoque/ProdutoAuxController.php
+++ src/Controller/Estoque/ProdutoAuxController.php
@@ -146,7 +146,7 @@ class ProdutoAuxController extends FormListController
         while ($produto = $qryProdutos->fetch()) {
             $novoDir = $crosierBaseDir . 'crosierapp-vendest/public/images/produtos/' . $produto['depto_id'] . '/' . $produto['grupo_id'] . '/' . $produto['subgrupo_id'] . '/';
             $this->logger->info($novoDir);
-            if (!mkdir($concurrentDirectory = $novoDir, 0777, true) && !is_dir($concurrentDirectory)) {
+            if (!file_exists($novoDir) && !mkdir($concurrentDirectory = $novoDir, 0777, true) && !is_dir($concurrentDirectory)) {
                 throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
             }
             rename($crosierBaseDir . 'crosierapp-vendest/public/images/produtos/5d/' . $produto['image_name'], $concurrentDirectory . $produto['image_name']);
