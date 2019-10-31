b9bfed3ffa53f6ae2f217c8416e4be3171ca8c95
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-10-30 23:09:54 -0300

    ...

--- sql/00-CREATE_DATABASE.sql
+++ sql/00-CREATE_DATABASE.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS `crosier_rodoponta_dev`;
 CREATE DATABASE `crosier_rodoponta_dev` CHARACTER SET 'utf8' COLLATE 'utf8_swedish_ci';
 GRANT ALL PRIVILEGES ON crosier_rodoponta_dev.* TO 'crosier_rodoponta_dev'@'localhost' IDENTIFIED BY 'crosier_rodoponta_dev';
 FLUSH PRIVILEGES;
