8f1667bda187fe245dcfc31bfe6e0ac09ad43fff
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-01-28 18:15:43 -0300

    0000004: CONSULTA SERASA POR WEBSERVICE PELO SISTEMA EKT

--- src/Controller/Vendas/API/ConsultaSPCAPIController.php
+++ src/Controller/Vendas/API/ConsultaSPCAPIController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Vendas\API;
 
 use CrosierSource\CrosierLibBaseBundle\Repository\Config\AppConfigRepository;
 use CrosierSource\CrosierLibBaseBundle\Utils\DateTimeUtils\DateTimeUtils;
+use CrosierSource\CrosierLibBaseBundle\Utils\StringUtils\StringUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -81,15 +82,35 @@ class ConsultaSPCAPIController extends AbstractController
             ]
         ]);
 
-        $sep = '|@|';
-
         $r = [];
-        $r[] = 'Consulta para ' . $result->consumidor->{'consumidor-pessoa-juridica'}->{'nome-comercial'} . ' (' . $result->consumidor->{'consumidor-pessoa-juridica'}->{'razao-social'} . ')';
+
+        $pj = $result->consumidor->{'consumidor-pessoa-juridica'} ?? false;
+        $cadastroNaoLocalizado = false;
+        if ($pj) {
+            if ($result->consumidor->{'consumidor-pessoa-juridica'}->{'razao-social'} === 'CADASTRO NAO LOCALIZADO') {
+                $cadastroNaoLocalizado = true;
+            } else {
+                $r[] = 'Consulta para ' . $result->consumidor->{'consumidor-pessoa-juridica'}->{'nome-comercial'} . ' (' . $result->consumidor->{'consumidor-pessoa-juridica'}->{'razao-social'} . ')';
+            }
+        } else {
+            if ($result->consumidor->{'consumidor-pessoa-fisica'}->{'nome'} === 'CADASTRO NAO LOCALIZADO') {
+                $cadastroNaoLocalizado = true;
+            } else {
+                $r[] = 'Consulta para ' . $result->consumidor->{'consumidor-pessoa-fisica'}->{'nome'} ?? '';
+            }
+        }
+        if ($cadastroNaoLocalizado) {
+            $r[] = 'Cadastro não localizado para o documento ' . StringUtils::mascararCnpjCpf($documento);
+            return new Response(implode('<br>', $r));
+        }
+
+
         $r[] = '';
 
         if ($result->{'alerta-documento'}->resumo->{'quantidade-total'} ?? false) {
 
-            $r[] = '>>> Alerta(s) para ' . $result->{'alerta-documento'}->resumo->{'quantidade-total'} . ' documento(s):';
+            $r[] = '>>> CONSULTA DE DOCUMENTOS: alerta(s) para ' . $result->{'alerta-documento'}->resumo->{'quantidade-total'} . ' documento(s):';
+            $r[] = '';
 
             if ($result->{'alerta-documento'}->resumo->{'quantidade-total'} === 1) {
                 $detAlertaDocumento = $result->{'alerta-documento'}->{'detalhe-alerta-documento'};
@@ -124,19 +145,20 @@ class ConsultaSPCAPIController extends AbstractController
                         ' (Incluído em ' . DateTimeUtils::parseDateStr(($detAlertaDocumento->{'data-inclusao'} ?? ''))->format('d/m/Y') . ') ';
                 }
             }
+        } else {
+            $r[] = '>>> CONSULTA DE DOCUMENTOS: Nenhum alerta de ocorrências.';
         }
 
         $r[] = '';
-        $r[] = '>>> CONSULTA SPC';
+        $r[] = '';
 
-        $totalOcorrencias = ($result->{'spc'}->resumo->{'quantidade-total'} ?? 0);
-
-
-        if ($totalOcorrencias) {
+        $totalOcorrenciasSPC = ($result->{'spc'}->resumo->{'quantidade-total'} ?? 0);
+        if ($totalOcorrenciasSPC) {
             $valorTotalOcorrencias = $result->{'spc'}->resumo->{'valor-total'} ?? 0;
-            $r[] = $totalOcorrencias . ' ocorrência(s) encontrada(s) no valor total de R$ ' . number_format($valorTotalOcorrencias, 2, ',', '.');
+            $r[] = '>>> CONSULTA SPC: ' . $totalOcorrenciasSPC . ' ocorrência(s) encontrada(s) no valor total de R$ ' . number_format($valorTotalOcorrencias, 2, ',', '.');
+            $r[] = '';
 
-            if ($totalOcorrencias === 1) {
+            if ($totalOcorrenciasSPC === 1) {
                 $detSpc = $result->{'spc'}->{'detalhe-spc'};
                 $r[] =
                     '1) Incluído por ' . ($detSpc->{'nome-entidade'} ?? '') .
@@ -158,8 +180,70 @@ class ConsultaSPCAPIController extends AbstractController
                         'Valor: ' . number_format($detSpc->{'valor'}, 2, ',', '.');
                 }
             }
+        } else {
+            $r[] = '>>> CONSULTA SPC: Nenhuma ocorrência.';
         }
 
+        $r[] = '';
+        $r[] = '';
+
+        $totalOcorrenciasPROTESTOS = ($result->{'protesto'}->resumo->{'quantidade-total'} ?? 0);
+        if ($totalOcorrenciasPROTESTOS) {
+            $valorTotalOcorrencias = $result->{'protesto'}->resumo->{'valor-total'} ?? 0;
+            $r[] = '>>> CONSULTA PROTESTOS: ' . $totalOcorrenciasPROTESTOS . ' ocorrência(s) encontrada(s) no valor total de R$ ' . number_format($valorTotalOcorrencias, 2, ',', '.');
+            $r[] = '';
+
+            if ($totalOcorrenciasPROTESTOS === 1) {
+                $detProtesto = $result->{'protesto'}->{'detalhe-protesto'};
+                $r[] =
+                    '1) Cartório: ' . ($detProtesto->cartorio->nome) . ' em ' . $detProtesto->cartorio->cidade->nome . '-' . $detProtesto->cartorio->cidade->estado->{'sigla-uf'} .
+                    ' em ' . DateTimeUtils::parseDateStr($detProtesto->{'data-protesto'})->format('d/m/Y') .
+                    'Valor: ' . number_format($detProtesto->{'valor'}, 2, ',', '.');
+
+            } else {
+                $i = 1;
+                foreach ($result->{'protesto'}->{'detalhe-protesto'} as $detProtesto) {
+                    $r[] =
+                        $i++ . ') Cartório: ' . ($detProtesto->cartorio->nome) . ' em ' . $detProtesto->cartorio->cidade->nome . '-' . $detProtesto->cartorio->cidade->estado->{'sigla-uf'} .
+                        ' em ' . DateTimeUtils::parseDateStr($detProtesto->{'data-protesto'})->format('d/m/Y') . '. ' . 
+                        'Valor: ' . number_format($detProtesto->{'valor'}, 2, ',', '.');
+                }
+            }
+        } else {
+            $r[] = '>>> CONSULTA PROTESTOS: Nenhuma ocorrência.';
+        }
+
+        $r[] = '';
+        $r[] = '';
+
+        $totalOcorrenciasChequeLojista = ($result->{'cheque-lojista'}->resumo->{'quantidade-total'} ?? 0);
+        if ($totalOcorrenciasChequeLojista) {
+            $valorTotalOcorrencias = $result->{'cheque-lojista'}->resumo->{'valor-total'} ?? 0;
+            $r[] = '>>> CONSULTA DE CHEQUES: ' . $totalOcorrenciasChequeLojista . ' ocorrência(s) encontrada(s) no valor total de R$ ' . number_format($valorTotalOcorrencias, 2, ',', '.');
+            $r[] = '';
+
+            if ($totalOcorrenciasChequeLojista === 1) {
+                $detChequeLojista = $result->{'cheque-lojista'}->{'detalhe-chequeLojista'};
+                $r[] =
+                    '1) Cartório: ' . ($detChequeLojista->cartorio->nome) . ' em ' . $detChequeLojista->cartorio->cidade->nome . '-' . $detChequeLojista->cartorio->cidade->estado->{'sigla-uf'} .
+                    ' em ' . DateTimeUtils::parseDateStr($detChequeLojista->{'data-'})->format('d/m/Y') .
+                    'Valor: ' . number_format($detChequeLojista->{'valor'}, 2, ',', '.');
+
+            } else {
+                $i = 1;
+                foreach ($result->{'cheque-lojista'}->{'detalhe-cheque-lojista'} as $detChequeLojista) {
+                    $r[] =
+                        $i++ . ') Alinea ' . $detChequeLojista->alinea->descricao .
+                        ' (' . str_pad($detChequeLojista->{'cheque-inicial'}->{'dados-bancarios'}->banco->codigo, 3, '0', STR_PAD_LEFT) . ' - ' . $detChequeLojista->{'cheque-inicial'}->{'dados-bancarios'}->banco->nome . '. ' .
+                        'Agência ' . $detChequeLojista->{'cheque-inicial'}->{'dados-bancarios'}->{'numero-agencia'} . '. ' .
+                        'Número: ' . $detChequeLojista->{'cheque-inicial'}->{'numero'} . '. ' .
+                        'Emissão: ' . DateTimeUtils::parseDateStr($detChequeLojista->{'cheque-inicial'}->{'data-emissao'})->format('d/m/Y') . '. ' .
+                        'Valor: ' . number_format($detChequeLojista->{'cheque-inicial'}->{'valor'}, 2, ',', '.');
+                }
+            }
+        } else {
+            $r[] = '>>> CONSULTA DE CHEQUES: Nenhuma ocorrềncia.';
+        }
 
         return new Response(implode('<br>', $r));
     }
