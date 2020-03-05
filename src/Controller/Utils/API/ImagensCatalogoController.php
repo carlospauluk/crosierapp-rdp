0dd16f4b6290eb288551b187caeb3ad91498484d
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-03-04 22:28:24 -0300

    ...

--- src/Controller/Utils/API/ImagensCatalogoController.php
+++ src/Controller/Utils/API/ImagensCatalogoController.php
@@ -115,28 +115,18 @@ class ImagensCatalogoController extends BaseController
             $imagensNoCatalogo['naoConsta'] = $imagensNoCatalogo['naoConsta'] ?? [];
             $imagensNoCatalogo['erros'] = $imagensNoCatalogo['erros'] ?? [];
 
-            $ids = array_merge([-1], $imagensNoCatalogo['baixadas'], $imagensNoCatalogo['erros'], $imagensNoCatalogo['naoConsta']);
+            $ids = array_merge([-1], /*$imagensNoCatalogo['baixadas'], */ $imagensNoCatalogo['erros'], $imagensNoCatalogo['naoConsta']);
 
             /** @var ProdutoRepository $repoProduto */
             $repoProduto = $this->getDoctrine()->getRepository(Produto::class);
             /** @var Connection $conn */
             $conn = $this->getDoctrine()->getConnection();
             $limit = (int)($request->get('limit') ?? 500);
-            $todos = $conn->fetchAll('SELECT id, json_data->>"$.recnum" as recnum FROM est_produto WHERE json_data->>"$.recnum" IS NOT NULL AND id NOT IN (' . implode(',',$ids) . ') ORDER BY id LIMIT ' . $limit);
+            $todos = $conn->fetchAll('SELECT id, json_data->>"$.recnum" as recnum FROM est_produto WHERE json_data->>"$.recnum" IS NOT NULL AND id NOT IN (' . implode(',', $ids) . ') ORDER BY id LIMIT ' . $limit);
             $client = new Client();
             $this->getLogger()->error('SOH ATIVANDO O LOG');
             foreach ($todos as $r) {
 
-                if (!$r['recnum'] ?? false) {
-                    $this->getLogger()->info('id = ' . $r['id'] . ' sem recnum');
-                    $imagensNoCatalogo['erros'][] = $r['id'];
-                    continue;
-                }
-
-                if (in_array($r['id'], $imagensNoCatalogo['baixadas'])) {
-                    $this->getLogger()->info('id = ' . $r['id'] . '( recnum = ' . $r['recnum'] . ') já foi baixada');
-                    continue;
-                }
                 if (in_array($r['id'], $imagensNoCatalogo['naoConsta'])) {
                     $this->getLogger()->info('id = ' . $r['id'] . '( recnum = ' . $r['recnum'] . ') não possui imagem no catálogo');
                     continue;
@@ -147,34 +137,51 @@ class ImagensCatalogoController extends BaseController
                     $this->getLogger()->info('Tentando baixar para o recnum ' . $r['recnum']);
                     $response = $client->request('GET', $urlAPICatalogo)->getBody()->getContents();
                     $json = json_decode($response, true);
-                    $foto = $json['fotos'][0] ?? null;
-                    if ($foto) {
-                        $this->getLogger()->info('Baixando foto para produtoId = ' . $r['id']);
+
+                    if (isset($json['fotos']) && count($json['fotos']) > 0) {
                         /** @var Produto $produto */
                         $produto = $repoProduto->find($r['id']);
-                        $urlFoto = $_SERVER['URL_FOTOS_CATALOGO'] . $foto;
-                        $this->getLogger()->info('url = ' . $urlFoto);
-                        file_put_contents(sys_get_temp_dir() . '/' . $foto, file_get_contents($urlFoto));
 
-                        $file = new UploadedFile(sys_get_temp_dir() . '/' . $foto, $foto, null, null, true);
+                        if (count($json['fotos']) <= $produto->imagens->count()) {
+                            $this->getLogger()->info('id = ' . $r['id'] . '( recnum = ' . $r['recnum'] . ') já foi baixada');
+                            continue;
+                        }
 
-                        $produtoImagem = new ProdutoImagem();
-                        $produtoImagem->setImageFile($file);
-                        // $produtoImagem->setImageName($foto);
-                        $produtoImagem->setProduto($produto);
-                        /** @var ProdutoImagem $produtoImagem */
-                        $produtoImagem = $produtoImagemEntityHandler->save($produtoImagem);
+                        $this->getLogger()->info('Baixando foto para produtoId = ' . $r['id']);
+
+                        $i = 0;
+                        foreach ($json['fotos'] as $foto) {
+                            $i++;
+                            if (in_array($r['id'], $imagensNoCatalogo['baixadas']) && $i === 1) {
+                                continue;
+                            }
+                            $urlFoto = $_SERVER['URL_FOTOS_CATALOGO'] . $foto;
+                            $this->getLogger()->info('url = ' . $urlFoto);
+                            file_put_contents(sys_get_temp_dir() . '/' . $foto, file_get_contents($urlFoto));
+
+                            $file = new UploadedFile(sys_get_temp_dir() . '/' . $foto, $foto, null, null, true);
+
+                            $produtoImagem = new ProdutoImagem();
+                            $produtoImagem->setImageFile($file);
+                            // $produtoImagem->setImageName($foto);
+                            $produtoImagem->setProduto($produto);
+                            /** @var ProdutoImagem $produtoImagem */
+                            $produtoImagemEntityHandler->save($produtoImagem);
+                            $produto->imagens->add($produtoImagem);
+                        }
 
                         $json_data = json_encode([
                             'qtde_imagens' => $produto->imagens->count(),
-                            'imagem1' => $produtoImagem->getImageName()
+                            'imagem1' => $produto->imagens->get(0)->getImageName()
                         ]);
 
                         $json_data = 'JSON_MERGE_PATCH(json_data,\'' . $json_data . '\')';
 
                         $conn->exec('UPDATE est_produto SET json_data = ' . $json_data . ' WHERE id = ' . $produto->getId());
-                        $imagensNoCatalogo['baixadas'][] = $r['id'];
-                        $this->getLogger()->info('OK');
+                        if (!in_array($r['id'], $imagensNoCatalogo['baixadas'])) {
+                            $imagensNoCatalogo['baixadas'][] = $r['id'];
+                        }
+                        $this->getLogger()->info('OK >>> ' . $i . ' imagens baixadas');
                     } else {
                         $this->getLogger()->info('NÃO CONSTA ' . $r['recnum']);
                         $imagensNoCatalogo['naoConsta'][] = $r['id'];
@@ -205,21 +212,21 @@ class ImagensCatalogoController extends BaseController
 
             $html .= '<h1 id="baixadas">Imagens baixadas do catálogo</h1>';
             foreach ($imagensNoCatalogo['baixadas'] as $k => $v) {
-                $html .= ($k+1) . ') <a href="' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '">' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '</a><br>';
+                $html .= ($k + 1) . ') <a href="' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '">' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '</a><br>';
                 $t++;
             }
             $html .= '<hr>';
 
             $html .= '<h1 id="naoConsta">Imagens que não constam no catálogo</h1>';
             foreach ($imagensNoCatalogo['naoConsta'] as $k => $v) {
-                $html .= ($k+1) . ') <a href="' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '">' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '</a><br>';
+                $html .= ($k + 1) . ') <a href="' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '">' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '</a><br>';
                 $t++;
             }
             $html .= '<hr>';
 
             $html .= '<h1 id="erros">Erros</h1>';
             foreach ($imagensNoCatalogo['erros'] as $k => $v) {
-                $html .= ($k+1) . ') <a href="' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '">' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '</a><br>';
+                $html .= ($k + 1) . ') <a href="' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '">' . $_SERVER['CROSIERAPPRADX_URL'] . '/est/produto/form/' . $v . '</a><br>';
                 $t++;
             }
             $html .= '<hr>';
@@ -229,7 +236,6 @@ class ImagensCatalogoController extends BaseController
             $this->getLogger()->error('Erro ao downloadCatalogo');
             $this->getLogger()->error($e->getMessage());
         }
-
 
 
         return new Response($html);
